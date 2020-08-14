4819546fae60169c7f97370bdc6678560c660fbe
rakshitpoonia <rakshitpoonia2004@gmail.com>
2020-08-14 18:27:12 +0530

    changes

--- sketch.js
+++ sketch.js
@@ -4,9 +4,9 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var box1, pig1,pig3;
+var box1, boy1,boy3;
 var backgroundImg,platform;
-var bird, slingshot;
+var boy2, slingshot;
 
 var gameState = "onSling";
 var bg = "sprites/bg1.png";
@@ -27,12 +27,12 @@ function setup(){
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
+    boy1 = new Boy1(810, 350);
     log1 = new Log(810,260,300, PI/2);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+    boy3 = new Boy1(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
 
@@ -40,10 +40,10 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(200,50);
+    boy2 = new Boy2(200,50);
 
     //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:50});
+    slingshot = new SlingShot(boy2.body,{x:200, y:50});
 }
 
 function draw(){
@@ -60,21 +60,21 @@ function draw(){
     box1.display();
     box2.display();
     ground.display();
-    pig1.display();
-    pig1.score();
+    boy1.display();
+    boy1.score();
     log1.display();
 
     box3.display();
     box4.display();
-    pig3.display();
-    pig3.score();
+    boy3.display();
+    boy3.score();
     log3.display();
 
     box5.display();
     log4.display();
     log5.display();
 
-    bird.display();
+    boy2.display();
     platform.display();
     //log6.display();
     slingshot.display();    
@@ -86,10 +86,10 @@ function draw(){
 
 function mouseDragged(){
     if (gameState!=="launched"){
-        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+        Matter.Body.setPosition(boy2.body, {x: mouseX , y: mouseY});
 }
 }
-
+  
 
 function mouseReleased(){
     slingshot.fly();
@@ -97,10 +97,10 @@ function mouseReleased(){
 }
 
 function keyPressed(){
-    if(keyCode === 32 && bird.body.speed < 1){
-      slingshot.attach(bird.body);  
-        Matter.Body.setPosition(bird.body, {x: 200 , y: 50});
-        bird.trajectory=[];
+    if(keyCode === 32 && boy2.body.speed > 1 || boy2.body.speed > 0.2 ){
+      slingshot.attach(boy2.body);  
+        Matter.Body.setPosition(boy2.body, {x: 200 , y: 50});
+        boy2.trajectory=[];
         gameState="onSling";
     }
    
@@ -109,18 +109,19 @@ function keyPressed(){
 
 
 async function getBackgroundImg(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Europe/London");
     var responseJSON = await response.json();
 
     var datetime = responseJSON.datetime;
     var hour = datetime.slice(11,13);
     
-    if(hour>=0600 && hour<=1900){
-        bg = "sprites/bg1.png";
-    }
-    else{
-        bg = "sprites/bg2.jpg";
-    }
+    //if(hour>=0600 && hour<=1900){
+        //bg = "sprites/bg1.png";
+    //}
+    //else{
+        //bg = "sprites/bg2.jpg";
+    //}
+    bg="sprites/bg1.png";
 
     backgroundImg = loadImage(bg);
     console.log(backgroundImg);
